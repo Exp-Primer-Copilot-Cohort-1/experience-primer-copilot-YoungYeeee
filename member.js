eb4f80c0c0a34d9862778023460f11dea31fbb45
trueyoungleaf <102295361+YoungYeeee@users.noreply.github.com>
2024-04-24 11:26:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/skills/skills.html',
+        controller: 'SkillsController',
+        controllerAs: 'skillsCtrl'
+    };
+}
